18f76ffea90d4f014da2034a88fa278fea93c44c
raremammadov-max <raremammadov@gmail.com>
2026-02-17 22:27:19 +0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,13 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./index.css";
 
 (window as any).MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
