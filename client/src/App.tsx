cf3d0cdef1e741afeefac9b271dfdfb7494802bd
raremammadov-max <raremammadov@gmail.com>
2026-02-18 10:28:44 +0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,47 @@
-// App.tsx
+// client/src/App.tsx
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
+
+import { queryClient } from "./lib/queryClient";
 import { Toaster } from "./components/ui/toaster";
-import { AuthProvider } from "@/hooks/use-auth";
-import { Navbar } from "@/components/Navbar";
+import { AuthProvider } from "./hooks/use-auth";
+import { Navbar } from "./components/Navbar";
 
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/Home";
-import AuthPage from "@/pages/AuthPage";
-import ProfilePage from "@/pages/ProfilePage";
-import PlacePage from "@/pages/PlacePage";
-import FavoritesPage from "@/pages/FavoritesPage";
-import RoutePage from "@/pages/RoutePage";
-import WelcomePage from "@/pages/WelcomePage";
+import NotFound from "./pages/not-found";
+import Home from "./pages/Home";
+import AuthPage from "./pages/AuthPage";
+import ProfilePage from "./pages/ProfilePage";
+import PlacePage from "./pages/PlacePage";
+import FavoritesPage from "./pages/FavoritesPage";
+import RoutePage from "./pages/RoutePage";
+import WelcomePage from "./pages/WelcomePage";
 
-// BLOG
-import BlogPage from "@/pages/BlogPage";
-import BlogPostPage from "@/pages/BlogPostPage";
+// ✅ BLOG
+import BlogPage from "./pages/BlogPage";
+import BlogPostPage from "./pages/BlogPostPage";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={WelcomePage} />
+
       <Route path="/blog" component={BlogPage} />
       <Route path="/blog/:slug" component={BlogPostPage} />
+
       <Route path="/map" component={Home} />
+
       <Route path="/auth" component={AuthPage} />
       <Route path="/profile" component={ProfilePage} />
       <Route path="/favorites" component={FavoritesPage} />
       <Route path="/route" component={RoutePage} />
       <Route path="/place/:id" component={PlacePage} />
+
       <Route component={NotFound} />
     </Switch>
   );
 }
 
-function App() {
+export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
@@ -49,5 +54,3 @@ function App() {
     </QueryClientProvider>
   );
 }
-
-export default App;
